9df52b52af413e4fa05403042e2d865b73873108
Demian <fluffy.little.ian@gmail.com>
2026-07-31 20:46:54 -0600

    Resultado redondeado

--- Programas_Basicos/promedio.py
+++ Programas_Basicos/promedio.py
@@ -3,5 +3,5 @@ print("Ingrese los tres números que desea promediar")
 n1=float(input("Primer número: "))
 n2=float(input("Segundo número: "))
 n3=float(input("Tercer número: "))
-promedio=(n1+n2+n3)/3
+promedio=round((n1+n2+n3)/3, 2)
 print("El promedio de los números que ingresó es ", promedio)
