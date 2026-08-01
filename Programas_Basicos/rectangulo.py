2127b9ca4a9c5716d5d8328b012e755c88a3be33
Demian <fluffy.little.ian@gmail.com>
2026-07-31 21:13:57 -0600

    Correcciones ortográficas

--- Programas_Basicos/rectangulo.py
+++ Programas_Basicos/rectangulo.py
@@ -1,7 +1,7 @@
 #obtener el área de u rectáculo en base
 #a los datos ingresados por el usuario
-print("Calculadora de l área de un rectágulo\nAsegúrese de que ambas medidas estén en la muisma escala.")
-base = float(input("INgrese la medida de la base del rectángulo: "))
-altura=float(input("INgrese la medida de la altura del rectángulo: "))
+print("Calculadora del área de un rectágulo\nAsegúrese de que ambas medidas estén en la muisma escala.")
+base = float(input("Ingrese la medida de la base del rectángulo: "))
+altura=float(input("Ingrese la medida de la altura del rectángulo: "))
 area=base*altura
 print("El área de su rectángulo es de ", area)
