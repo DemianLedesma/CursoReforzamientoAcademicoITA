1f207edbb1dca555b6a820266feb6613342bbf9e
Demian <fluffy.little.ian@gmail.com>
2026-08-15 19:10:07 -0600

    añadido funcionamiento para sólo aplicar a una palabra si el usuario ingresa varias

--- Programas_Bucles/count_a.py
+++ Programas_Bucles/count_a.py
@@ -2,7 +2,9 @@
 #contar las letras a en una palabra
 palabra=input("Ingrese una palabra: ").lower()
 contador=0
-
+if len(palabra.split(" "))>1:
+    print("Sólo se tomará la primera palabra escrita en cuenta.")
+    palabra=palabra.split(" ")[0]
 for l in palabra:
     if l=="a":
         contador+=1
