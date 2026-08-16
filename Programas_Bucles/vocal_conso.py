ccf446cd51db81e9631ee82c206a74919441ea4d
Demian <fluffy.little.ian@gmail.com>
2026-08-15 19:42:23 -0600

    Añadida validación por si el usuario ingresa más de una letra o algo que no sea letra

--- Programas_Bucles/vocal_conso.py
+++ Programas_Bucles/vocal_conso.py
@@ -1,10 +1,17 @@
 # bucle while
 # mostrar cuando la letra que ingresa el usuario es vocal o consonante
 while True:
-    letra=input("Ingrese una letra: ").lower()
-    if letra==" ": break
-    if letra in "aeiou":
-        print("La letra", letra, "es una vocal.")
-    else:
-        print("La letra", letra, "es una consonante.")
-print("Programa terminado.")
+    l= input("Letra:")
+    if l==" ":
+        print("Programa terminado.")
+        break
+    letra= l.lower() if len(l)==1 and l.isalpha() else (False if l.isalpha()==False else None)
+    if letra:
+        if letra in "aeiou":
+            print("La letra", letra, "es vocal.")
+        else:
+            print("La letra", letra, "es consonante.")
+    elif letra==False:
+        print("Por favor, ingrese un caracter alfabético.")
+    elif letra==None:
+        print("Por favor, ingrese sólo una letra.") 
