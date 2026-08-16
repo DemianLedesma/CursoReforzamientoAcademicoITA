9605bb96ed7d3ac6e527a6f35e199e3b1905af9c
Demian <fluffy.little.ian@gmail.com>
2026-08-15 18:41:38 -0600

    Funcionamiento de mostrar números impares añadida

---
+++ Programas_Bucles/odd_count.py
@@ -0,0 +1,13 @@
+# bucle while
+# mostrar los números impares hasta el que ingresa el usuario
+
+n=int(input("INgrese el número tope: "))
+i=1
+while True:
+    if i%2!=0:
+        print(i, ", ")
+    i+=1
+    if i>n:
+        break
+
+print("Se han mostrado todos los números impares hasta", n)
