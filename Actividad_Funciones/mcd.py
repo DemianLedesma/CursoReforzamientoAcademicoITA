f63f1d8290652afe39fc326f3dbb4dec49442783
Demian <fluffy.little.ian@gmail.com>
2026-08-18 17:14:00 -0600

    Comparación de resultados y correcciones hechas

--- Actividad_Funciones/mcd.py
+++ Actividad_Funciones/mcd.py
@@ -3,13 +3,21 @@ from math import gcd
 def mcd(n1,n2):
     a=abs(n1)
     b=abs(n2)
-    if a==0 or b==0:
+    if a==0 and b==0:
         return 0
     while b!=0:
         a,b=b,a%b
     return a
 
-print(mcd(
-    int(input("Ingrese su primer número: ")),
-    int(input("Ingrese su segundo número: "))
-    ))
+print("Calcular el Mínimo Común Divisor")
+a=int(input("Ingrese su primer número: "))
+b=int(input("Ingrese su segundo número: "))
+mcdc=mcd(a,b)
+mcdm=gcd(a,b)
+print(f"\nMCD({a}, {b}) = {mcdc}")
+
+print("\nComparar con la función de MATH")
+print("MCDc : MCD calculado \nMCDm : MCD de 'math'\n")
+print(f"MCDc={mcdc} \nMCDm={mcdm}")
+print("Los resultados coinciden." if mcdc==mcdm else "Los resultados no coinciden.")
+print("Fin del programa")
