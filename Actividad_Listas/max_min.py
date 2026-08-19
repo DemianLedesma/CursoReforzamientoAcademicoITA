a14070d608602b7a43711b051021cea25b33ca60
Demian <fluffy.little.ian@gmail.com>
2026-08-19 13:05:08 -0600

    Funcionalidad para que el usuario ingrese 8 números y se caloren para obtener el máximo y mínimo de esos, y luego validar con el sistema los resultados

--- Actividad_Listas/max_min.py
+++ Actividad_Listas/max_min.py
@@ -15,3 +15,16 @@ def min_calc(numeros):
         if num<m:
             m=num
     return m
+
+nums=[]
+for i in range(8):
+    v=int(input(f"Número {i+1}: "))
+    nums.append(v)
+
+maximo, minimo=max_calc(nums),min_calc(nums)
+print(f"\nNúmero mayor ingresado \nCalculado: {maximo} \nSistema: {max(nums)}",
+ "\nCoinciden los máximos." if maximo==max(nums) else "No coinciden.")
+
+print(f"\nNúmero menor ingresado \nCalculado: {minimo} \nSistema: {min(nums)}",
+ "\nCoinciden los mínimos." if minimo==min(nums) else "No coinciden.")
+print("\n\nFin del programa.")
